52b9af3ff58e892d5a42bbe2195927d23adc0b98
Jerin Easo Regi <jerin.easo.20@gmail.com>
2020-10-01 11:27:24 +0530

    Updated every functions

--- javascript/app.js
+++ javascript/app.js
@@ -3,21 +3,3 @@ document.querySelector('.ham').addEventListener('click',() =>{
     document.querySelector('.menu').classList.toggle('is-active');
 });
 
-//Form Submission 
-$("#submit-form").submit((e)=>{
-    e.preventDefault()
-    $.ajax({
-        url:"https://script.google.com/macros/s/AKfycbzKCXNOvTR8q2NnJZx_GHczpMkxCOEZ9Q1RuvOs_Q/exec",
-        data:$("#submit-form").serialize(),
-        method:"post",
-        success:function (response){
-            alert("Form submitted successfully")
-            window.location.reload()
-            //window.location.href="https://google.com"
-        },
-        error:function (err){
-            alert("Something Error")
-
-        }
-    })
-})
