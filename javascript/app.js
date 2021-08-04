ecfa20e29e19f45f37e898e8c1cd1e80d87bc9a1
jerineaso <jerin.easo.20@gmail.com>
2021-08-04 23:03:49 +0530

    updated portfolio

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,10 @@
 //Navbars Toggle 
 document.querySelector('.ham').addEventListener('click',() =>{
-    document.querySelector('.menu').classList.toggle('is-active');
+    document.querySelector('.menu').classList.toggle('show');
 });
 
+document.querySelectorAll('.links').forEach((key)=>{
+    key.addEventListener('click',()=>{
+        document.querySelector('.menu').classList.remove('show');
+    })
+})
