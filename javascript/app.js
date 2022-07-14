6b67fb2a924f5f340ee7aecad079e820847b0c2a
jerineaso <jerin.easo.20@gmail.com>
2022-07-14 22:43:12 +0530

    countries added

--- javascript/app.js
+++ javascript/app.js
@@ -8,3 +8,27 @@ document.querySelectorAll('.links').forEach((key)=>{
         document.querySelector('.menu').classList.remove('show');
     })
 })
+
+// API for getting countries list
+async function getResponse() {
+	const response = await fetch(
+		'https://countriesnow.space/api/v0.1/countries/capital',
+		{
+			method: 'GET'
+		}
+	);
+	const data = await response.json(); // Extracting data as a JSON Object from the response
+    const current = data.data
+
+    let drops = '';
+
+    current.forEach(country =>{
+        console.log(country.name);
+        drops += `<option class="opt">${country.name}</option>`
+    })
+
+    document.getElementById("country").innerHTML = drops
+    
+}
+
+getResponse();
